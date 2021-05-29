9edf5e998f1e8015e878dee7d9442dd410b85858
Warwick Stone <warwick_stone@fastmail.com>
2021-05-29 16:09:19 +1000

    WIP: specify solution root

--- sln_graph.py
+++ sln_graph.py
@@ -1,10 +1,12 @@
+#!python
+
 from glob import glob
 import json
 import xml.etree.ElementTree as et
 
-def find_opt_csprojs():
+def find_opt_csprojs(root):
   """ find all csproj files under the current dir """
-  return glob('*/**/*.csproj')
+  return glob(f'{root}/**/*.csproj')
 
 def get_all_project_refs(csproj_path):
   """ return all csproj files referenced by the given csproj """
@@ -12,11 +14,11 @@ def get_all_project_refs(csproj_path):
   for ref in proj.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ProjectReference'):
     yield ref.attrib['Include']
 
-def build_csproj_adj_list():
+def build_csproj_adj_list(root):
   # returns dict of proj filename -- references --> [proj filename]
   # NOTE: no paths - just filenames
   adj = {}
-  for proj_path in find_opt_csprojs():
+  for proj_path in find_opt_csprojs(root):
     proj_filename = proj_path.split('\\')[-1]
     if proj_filename in adj:
       raise Exception(proj_filename + ' already in adj list!')
@@ -38,7 +40,9 @@ def build_json_graph(adj_list):
 
 
 if __name__ == '__main__':
-  adj = build_csproj_adj_list()
+  import sys
+  root = sys.argv[1] if len(sys.argv) == 2 else '.'
+  adj = build_csproj_adj_list(root)
   assert_all_refs_in_adj_list(adj)
   jobj = build_json_graph(adj)
   print(json.dumps(jobj))
