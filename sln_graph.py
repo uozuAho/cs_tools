fbf463a155a8a0d74a0eaeec3122fa1440173346
Warwick Stone <warwick_stone@fastmail.com>
2021-05-29 16:04:26 +1000

    init with sln_graph

---
+++ sln_graph.py
@@ -0,0 +1,44 @@
+from glob import glob
+import json
+import xml.etree.ElementTree as et
+
+def find_opt_csprojs():
+  """ find all csproj files under the current dir """
+  return glob('*/**/*.csproj')
+
+def get_all_project_refs(csproj_path):
+  """ return all csproj files referenced by the given csproj """
+  proj = et.parse(csproj_path)
+  for ref in proj.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ProjectReference'):
+    yield ref.attrib['Include']
+
+def build_csproj_adj_list():
+  # returns dict of proj filename -- references --> [proj filename]
+  # NOTE: no paths - just filenames
+  adj = {}
+  for proj_path in find_opt_csprojs():
+    proj_filename = proj_path.split('\\')[-1]
+    if proj_filename in adj:
+      raise Exception(proj_filename + ' already in adj list!')
+    adj[proj_filename] = list(p.split('\\')[-1] for p in get_all_project_refs(proj_path))
+  return adj
+
+def assert_all_refs_in_adj_list(adj_list):
+  """ Throw an exception if any project references an unknown project """
+  for proj in adj_list:
+    for adj in adj_list[proj]:
+      if adj not in adj_list:
+        raise Exception('{} references {}, which was not found'.format((proj, adj)))
+
+def build_json_graph(adj_list):
+  jobj = { 'nodes': [{'id': p, 'label': p} for p in adj_list], 'edges': [] }
+  for p in adj_list:
+    jobj['edges'].extend([{'from': p, 'to': adj} for adj in adj_list[p]])
+  return jobj
+
+
+if __name__ == '__main__':
+  adj = build_csproj_adj_list()
+  assert_all_refs_in_adj_list(adj)
+  jobj = build_json_graph(adj)
+  print(json.dumps(jobj))
